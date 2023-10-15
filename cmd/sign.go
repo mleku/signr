f5ff0d51ab60603cdad3d13b7db805c18135f7aa
mleku <mleku@gfy>
2023-10-15 14:50:53 -0100

    added help notes about signature format and hex/sigonly options

--- cmd/sign.go
+++ cmd/sign.go
@@ -19,6 +19,8 @@ var signCmd = &cobra.Command{
 Use the filename '-' to indicate that the data is being piped in with < or |.
 
 If filename is in fact a 64 character hexadecimal value, it will be signed on without the nonce and public key, and yield only a bech32 signature, or hexadecimal with the --hex flag.
+
+The sigonly option is the same as the hex option except the output signature is in nostr nsig Bech32 format.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -42,7 +44,7 @@ func init() {
 	signCmd.PersistentFlags().StringVarP(&Custom, "custom", "k", "",
 		"custom additional namespace")
 	signCmd.PersistentFlags().BoolVarP(&Hex, "hex", "x", false,
-		"print signature in hex")
+		"print signature in hex - this also applies the same effect as sigonly")
 	signCmd.PersistentFlags().BoolVarP(&OnlySig, "sigonly", "s", false,
 		"print only signature - note: this also omits the adding of a " +
 		"nonce as a verifier could not know it")
